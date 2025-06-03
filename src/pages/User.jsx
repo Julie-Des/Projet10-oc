3266f85d38567b82e4b62b52ff5cc82458d59f32
Julie-Des <julie.deshayes14@gmail.com>
2025-06-03 16:14:50 +0200

    feat: add in User redirection to SignIn if no token

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,71 +1,69 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { getUserProfile } from "../redux/user/userSlice";
 import Account from "../components/Account";
 import EditNameForm from "../components/EditNameForm";
 import accountsData from "../Data/accountsData.json";
 
 function User() {
-  const dispatch = useDispatch();
-  const { token } = useSelector((state) => state.auth);
-  const { userInfo } = useSelector((state) => state.user);
+	const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+	const { token } = useSelector((state) => state.auth);
+	const { userInfo } = useSelector((state) => state.user);
 
-  const [isEditing, setIsEditing] = useState(false);
+	const [isEditing, setIsEditing] = useState(false);
 
-  useEffect(() => {
-    if (token && !userInfo) {
-      dispatch(getUserProfile(token));
+	useEffect(() => {
+    if(!token) {
+      navigate("/sign-in");
+      return;
     }
-  }, [dispatch, token, userInfo]);
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
+		if (!userInfo) {
+			dispatch(getUserProfile(token));
+		}
+	}, [token, userInfo, dispatch, navigate]);
 
-  const handleCancel = () => {
-    setIsEditing(false);
-  };
+	const handleEditClick = () => {
+		setIsEditing(true);
+	};
 
-  const handleSave = () => {
-    setIsEditing(false);
-  };
+	const handleCancel = () => {
+		setIsEditing(false);
+	};
 
-  if (!userInfo) return null;
+	const handleSave = () => {
+		setIsEditing(false);
+	};
 
-  return (
-    <>
-      <div className="header">
-        {!isEditing ? (
-          <>
-            <h2>
-              Welcome back,
-              <br />
-              {userInfo.userName}!
-            </h2>
-            <button className="edit-button" onClick={handleEditClick}>
-              Edit Name
-            </button>
-          </>
-        ) : (
-          <EditNameForm
-            userInfo={userInfo}
-            token={token}
-            onCancel={handleCancel}
-            onSave={handleSave}
-          />
-        )}
-      </div>
+	if (!userInfo) return null;
 
-      {accountsData.map((account, index) => (
-        <Account
-          key={index}
-          title={account.title}
-          amount={account.amount}
-          description={account.description}
-        />
-      ))}
-    </>
-  );
+	return (
+		<>
+			<div className="header">
+				{!isEditing ? (
+					<>
+						<h2>
+							Welcome back,
+							<br />
+							{userInfo.userName}!
+						</h2>
+						<button className="edit-button" onClick={handleEditClick}>
+							Edit Name
+						</button>
+					</>
+				) : (
+					<EditNameForm userInfo={userInfo} token={token} onCancel={handleCancel} onSave={handleSave} />
+				)}
+			</div>
+
+			{accountsData.map((account, index) => (
+				<Account key={index} title={account.title} amount={account.amount} description={account.description} />
+			))}
+		</>
+	);
 }
 
 export default User;
